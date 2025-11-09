fcc51bf8b38c8dabb874134216bc222a7b446608
om rajpal <omrajpal.exe@gmail.com>
2025-11-10 03:23:24 +0530

    minor bug fix

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -30,7 +30,7 @@ export function LoginForm({
     const res = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       },
     });
 
@@ -49,7 +49,7 @@ export function LoginForm({
       options: {
         emailRedirectTo:
           process.env.NEXT_PUBLIC_SITE_URL + "/auth/callback" ||
-          "http://localhost:3000/auth/callback",
+          "process.env.NEXT_PUBLIC_SITE_URL/auth/callback",
       },
     });
 
